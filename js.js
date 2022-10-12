6c4508c05779673fb90f05ec8ada84ed5842064c
Sebastian <110424482+si390@users.noreply.github.com>
2022-10-12 09:42:18 -0300

    gaspo

---
+++ js.js
@@ -0,0 +1,11 @@
+let misDatos = {
+    nombre: 'Gaspar',
+    apellido: 'de Cristobal',
+    dni: 45753076,
+    comidadfavoritas:['milanesa', 'fideos', 'pasteldepapa'
+    ],
+    age: 19,
+    saludar: function(){
+        return 'Hola mi nombre es'+ this.nombre + 'y tengo' + this.age + 'años. Mi primer comida favorita es' 
+    }
+}
