b1f890633a70f8372f8774c167d951bcd1f64e89
Sebastian <110424482+si390@users.noreply.github.com>
2022-10-14 00:38:43 -0300

    auto

--- js.js
+++ js.js
@@ -9,3 +9,18 @@ let misDatos = {
         return 'Hola mi nombre es'+ this.nombre + 'y tengo' + this.age + 'años. Mi primer comida favorita es' 
     }
 }
+
+let auto = {
+    marca: 'Toyota',
+    modelo: 'Corolla',
+    anio: '2021',
+    color: 'gris',
+    posicion: 0,
+    avanzar: function(n) {
+        this.posicion = this.posicion + n;
+        return 'La posicion es' + this.posicion;
+    }
+}
+
+console.log(auto.avanzar(3))
+console.log(auto.avanzar(2))
